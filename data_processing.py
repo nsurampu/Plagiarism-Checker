c5d8db230b78241ba3aae86f4ffad0cc4a07d66c
Naren Surampudi <nsurampu@gmail.com>
2018-09-17 23:28:20 +0530

    Added documentation for code

--- data_processing.py
+++ data_processing.py
@@ -6,12 +6,35 @@ import os
 import pickle
 import math
 
-stop_words = stopwords.words('english')
-tokenizer = RegexpTokenizer(r'\w+')
 
-ps = PorterStemmer()
+"""
+This file Contains Classes for processing data.
+
+"""
 class ParseAndStore:
+    """
+    This class is used to handle the processing of text files encoded in ASCII and processes them, created a dictionary of terms and their
+    frequencies and stores it as a pickle file (.pkl)
+    """
     def parseAndStore(self,folder, files):
+        """
+        Creates a dictionary having the terms with their term-frequencies.
+        This takes two parameters viz folder- folder where the files lie the and files- the
+        list of files that need to be processed. The processed data will be stored as a pickled file.
+
+        @type folder: string
+        @param folder: folder to access files.
+
+        @type files: list
+        @param files: the list of files which will be read.
+
+        @rtype: string
+        @return: success after the process terminates successfully
+
+        """
+        ps = PorterStemmer()
+        stop_words = stopwords.words('english')
+        tokenizer = RegexpTokenizer(r'\w+')
         # print(files)
         file_data = {}
         for file in files:
@@ -33,30 +56,46 @@ class ParseAndStore:
                     data_with_tf[keys[i]] = filtered.count(keys[i])
                 # print(data_with_tf)
             file_data[file] = data_with_tf
-
         cwd = os.getcwd()
-        with open(cwd+'\\' + 'data.txt','wb') as f:
+        with open(cwd +'\\' + 'data.txt','wb') as f:
             pickle.dump(file_data,f)
         return True
 
+
+
 folder = 'D:\\Plagiarism-Checker\\corpus-original\\'
-
-
 parse = ParseAndStore()
 files = os.listdir(folder)
 if parse.parseAndStore(folder,files):
     print('success')
 
 
-# # read data
-# with open(os.getcwd() +'\\' + 'data.txt','rb') as f:
-#         data = pickle.load(f)
-#         keys = data.keys()
-#         for key in keys:
-#             print(key)
 
 class TF_IDF:
+
+    """
+    The class calculates the tf-idf value while parsing each term of each document
+    and calculating the document frequency fo each word. Then the idf value is calculated
+    using document frequency and is multiplied with term frequency to produce tf-idf.
+    """
+
     def tf_idf(self,file_path):
+        """
+        The function calculates the tf-idf value while parsing each term of each document
+        and calculating the document frequency fo each word. Then the idf value is calculated
+        using document frequency and is multiplied with term frequency to produce tf-idf
+
+        @type folder: string
+        @param folder: folder to access files.
+
+        @type files: string
+        @param files: the path of the data.txt dictionary
+
+        @rtype: string
+        @return: path path of pickled dictionary containing tf-idf values
+
+        """
+
         with open(file_path,'rb') as f:
             corpus_dict = pickle.load(f)
             keys = corpus_dict.keys()
